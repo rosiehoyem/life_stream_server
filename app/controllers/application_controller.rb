2b566ab2c4721ee3ba56d9fd90d2cb2d711331e6
Rosanne Hoyem <rosiehoyem@Rosannes-MacBook-Air.local>
2016-11-20 22:14:56 -0600

    added devise views

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  def verify_is_super_admin
+    (current_user.nil?) ? (render :file => "#{Rails.root}/public/404", :layout => false, :status => :not_found) : ((render :file => "#{Rails.root}/public/404", :layout => false, :status => :not_found) unless current_user.role == "Super Admin")
+  end
 end
